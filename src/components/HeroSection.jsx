d4ca9c5767dbb5b6516cb0786999da194eb1e19f
Muthu404200 <muthu225420@gmail.com>
2025-11-04 16:59:08 +0530

    3d commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,6 +10,26 @@ const HeroSection = () => {
     >
       <div className="mx-auto w-full max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 md:grid md:grid-cols-2 md:items-center md:gap-4 lg:px-8 lg:py-32 overflow-x-hidden">
 
+        {/* Right Side - Spline 3D Scene */}
+        <motion.div
+          className="mt-6 md:mt-0 sm:p-10"
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: false, amount: 0.2 }}
+          transition={{
+            duration: 1,
+            delay: 0.4,
+            type: "spring",
+            stiffness: 100,
+            damping: 20,
+          }}
+        >
+          <div className=" w-full h-[400px] md:h-[400px] lg:h-[500px] rounded-lg overflow-hidden">
+          <Spline scene="https://prod.spline.design/E9MfGQQyBWbwPWM8/scene.splinecode" />
+          </div>
+        </motion.div>
+
+        
         {/* Left Content */}
         <motion.div
           className="max-w-prose text-left"
@@ -43,24 +63,6 @@ const HeroSection = () => {
           </motion.p>
         </motion.div>
 
-        {/* Right Side - Spline 3D Scene */}
-        <motion.div
-          className="mt-6 md:mt-0 sm:p-10"
-          initial={{ opacity: 0, scale: 0.9 }}
-          whileInView={{ opacity: 1, scale: 1 }}
-          viewport={{ once: false, amount: 0.2 }}
-          transition={{
-            duration: 1,
-            delay: 0.4,
-            type: "spring",
-            stiffness: 100,
-            damping: 20,
-          }}
-        >
-          <div className="w-full h-[400px] md:h-[400px] lg:h-[500px] rounded-lg overflow-hidden">
-            <Spline scene="https://prod.spline.design/E9MfGQQyBWbwPWM8/scene.splinecode" />
-          </div>
-        </motion.div>
       </div>
     </section>
   );
